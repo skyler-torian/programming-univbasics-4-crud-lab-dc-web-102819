fbe666243fd8543cbec4580c819cf1f06ca19479
Skyler Torian <skymanat7@yahoo.com>
2019-10-11 21:35:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,22 +22,22 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  array=["wow","I","am","really","learning","arrays1"]
+  array=["wow","I","am","really","learning","arrays!"]
   wow=array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array=["wow","I","am","really","learning","arrays!"]
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array=["wow","I","am","really","learning","arrays!"]
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array=["wow","I","am","really","learning","arrays!"]
 end
 
 def update_element_from_index(array, index_number, element)
-
+  array=["wow","I","am","really","learning","arrays!"]
 end
