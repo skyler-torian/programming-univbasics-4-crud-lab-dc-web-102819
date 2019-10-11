5a8c5ef8a4f1c3e1489cedaf974ebcbc6b7ef67e
Skyler Torian <skymanat7@yahoo.com>
2019-10-11 21:10:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   array=["Wow","I","am","really","learning"]
-  array << "arrays"
+  array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
